98875d6b5b95907ee47fbe55fe86cd31816e2b14
LuQ232 <piotrbednarek99@gmail.com>
2020-05-04 21:42:36 +0200

    Lab 1.1 Zadanie 3

--- Laboratorium_1.1_Zadanie_3/main.c
+++ Laboratorium_1.1_Zadanie_3/main.c
@@ -1,13 +1,20 @@
 #include "SET.h"
 
 int main() {
-	sbi(DDRD, PD7);
 
-	while (1) {
-		sbi(PORTD, PD7); // 1
-		_delay_ms(500);
-		cbi(PORTD, PD7); // 0
-		_delay_ms(500);
+	int array [] = {0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000};// tablica z wartoœciami 1 dla kolejnych diod
+	DDRD = 0b11111111; //Ustawia wyprowadzenia od PD0 do PD7 do pracy jako wyjscie
+	int i = 0, direction = -1; // ustawienie indeksu dla tablicy oraz poczatkowego kierunku
+	for (i = 0;; i += direction)
+	{
 
+		if ( i == 0 || i == 7 )
+		{
+			direction *= -1;	//Zmiana kierunku
+		}
+
+		PORTD = array[i];   // Zapalenie odpowiedniej diody
+		_delay_ms(500);   	// Program czeka 0.5 sekundy
 	}
+
 }
