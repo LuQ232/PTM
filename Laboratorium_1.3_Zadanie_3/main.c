104b582369e58e5ce491e0450c72fbdd7de07fab
LuQ232 <piotrbednarek99@gmail.com>
2020-05-28 17:12:56 +0200

    Lab 1.3 Zadanie 3

--- Laboratorium_1.3_Zadanie_3/main.c
+++ Laboratorium_1.3_Zadanie_3/main.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <avr/io.h>
 #include <util/delay.h>
 #include <avr/sfr_defs.h>
 #include <math.h>
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
-
-
 
 #ifndef _BV
 #define _BV(bit)				(1<<(bit))
@@ -49,45 +46,27 @@
 #define MAX(a,b)			((a>b)?(a):(b))
 #define ABS(x)				((x>0)?(x):(-x))
 
-
-#define START_TIMER0 TCCR1B |= (1 << WGM12) //wlacza timera
-#define STOP_TIMER0  TCCR1B &= 0B11111000 //stopuje timer
-
-uint8_t first_press = 0; // Czy guzik zostal wcisniety pierwszy raz
-uint8_t counter = 0; // licznik potrzebny do wyswietlania cyfr na 7-seg.
-uint8_t timer_working = 0; // zmienna informujaca o pracy licznika
-
 volatile uint8_t minuty, sekundy;
 volatile uint16_t liczba7Seg;
-
+volatile uint8_t counter = 0; // licznik potrzebny do wyswietlania cyfr na 7-seg.
+volatile uint8_t first_press = 0;
 volatile char znaki[4];
 
 char cyfra[10] = { 0b1111110, 0b0110000, 0b1101101, 0b1111001,0b0110011,
 		0b1011011,0b1011111, 0b1110000, 0b1111111,0b1111011};
 
+
 //Inicjalizacja Timer1 do wywolywania przerwania z czêstotliwoœci¹ 2Hz
 void TimerInit() {
-
 	//Wybranie trybu pracy CTC z TOP OCR1A
-	TCCR1B |= (1 << WGM12);
+	sbi(TCCR1B,WGM12);
 	//Wybranie dzielnika czestotliwosci
-	TCCR1B |= (1 << CS12);
-	//Zapisanie do OCR1A wartosci odpowiadajacej 1s
+	sbi(TCCR1B,CS12);  // dzielnik 256
+	//sbi(TCCR1B,);
+	//Zapisanie do OCR1A wartosci odpowiadajacej 0,5s
 	OCR1A = 15625;
 	//Uruchomienie przerwania OCIE1A
-	TIMSK |= (1 << OCIE1A);
-}
-
-//Inicjalizacja Przycisku do wywolywania przerwania
-void ButtonInit()
-{
-	//General Interrupy Controller Register
-	// uruchomienie przerwania dla przycisku
-	GICR = 0b01000000; // ustawienie bitu 6 (INT0)
-
-	//MCU CONTROL REGISTER
-	MCUCR = 0x02; // ustawienie wyzwalacza zbocza opadaj¹cego
-	// FALLING EDGE TRIGGER to 0x02
+	sbi(TIMSK,OCIE1A);
 }
 
 //Inicjalizacja portow do obs³ugi wyswietlacza 7 segmentowego
@@ -98,66 +77,63 @@ void seg7Init() {
 	PORTC= 0b00000000;
 }
 
+void button_init()
+{
+	//General Interrupy Controller Register
+	// uruchomienie przerwania dla przycisku
+	sbi(GICR,INT0);	// ustawienie pinu PD2 (INT0)
+
+	//MCU CONTROL REGISTER
+	sbi(MCUCR,ISC11); // ustawienie wyzwalacza zbocza opadaj¹cego
+}
+
 //Wyswietla na wyswietlaczu 7 segmentowym cyfre z argumentu
 void seg7ShowCyfra(uint8_t cyfraDoWyswietlenia) {
 	PORTC = cyfra[cyfraDoWyswietlenia]; //odpowiednie ustawienie calego portu Cs
 }
 
 
-//Funkcja uruchamiana z przerwaniem po przepelnieniu licznika w timer1
-ISR(TIMER1_COMPB_vect) {
+int main() {
+	sbi(DDRB,PB0);
+	seg7Init();
+	button_init();
 
-	if(counter == 10) // jezeli odliczanie dojdzie do zera
+	sei();
+	while(1)
 	{
-		STOP_TIMER0;  // zatrzymanie timer'a
-		timer_working = 0; // zmiana statusu o pracy timera
-		tbi(PORTD,PD0); // zapalenie diody led
-		_delay_ms(5000); // odczekanie 5s
-		tbi(PORTD,PD0); // zgaszenie diody led
-		counter = 0; // wyzerowanie licznika
-		TimerInit(); // Wlaczenie timera
 	}
-
-	seg7ShowCyfra(9-counter); // wyswietlenie cyfry na wysw. 7-seg
-	counter+=1; // inkrementacja licznika
+	return 0;
 }
-
 //Funkcja uruchamiana z przerwaniem po nacisnieciu przycisku
-ISR(INT0_vect)
-{
+ISR(INT0_vect){
 	if(first_press == 0) // jezeli to pierwsze nacisniecie przycisku
 	{
 		TimerInit(); // wlaczenie timera
 		first_press = 1; // zmiana statusu
-		timer_working = 1; // zmiana statusu timera
-	}else if(timer_working == 1) // jezeli timer pracuje
+	}else if(first_press == 1)  // jezeli to nie pierwsze nacisniecie przycisku( timer pracuje)
 	{
-		STOP_TIMER0; // zatrzymanie timera
-		timer_working = 0; // zmiana statusu timera
-	}else if(timer_working == 0) // jezeli timer nie pracuje
-	{
+		cbi(TIMSK,OCIE1A); // zatrzymanie timera
 		counter = 0; // wyzerowanie licznika
-		TimerInit(); // wlaczenie timera
-		timer_working = 1; // zmiana statusu timera
-	}
-}
-
-
-int main() {
-	DDRD = 0b00000001; //Ustawia wyprowadzenie PD0 do pracy jako wyjscie
-	PORTD = 0b00000000; // Ustawia stan niski na ca³ym porcie D
-
-	seg7Init(); // inicjalizacja wyswietlacza 7-segm.
-
-	ButtonInit(); // //Inicjalizacja Przycisku do wywolywania przerwania
-
-
-	sei(); //funkcja uruchamia globalne przerwania
-
-	while (1) {
+		first_press = 0; // zmiana statusu
 	}
 
-	return 0;
 }
 
+//Funkcja uruchamiana z przerwaniem po przepelnieniu licznika w timer1
+ISR(TIMER1_COMPA_vect) {
 
+	if(counter >= 10) // jezeli odliczanie dojdzie do zera
+		{
+			sbi(PORTB,PB0); // zapalenie diody led
+			if(counter == 15)
+			{
+				cbi(PORTB,PB0); // zgaszenie diody led
+				counter = -1; // wyzerowanie licznika
+			}
+		}else
+		{
+			seg7ShowCyfra(9-counter); // wyswietlenie cyfry na wysw. 7-seg
+		}
+	counter+=1; // inkrementacja licznika
+
+}
